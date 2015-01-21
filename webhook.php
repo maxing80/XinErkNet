7fb3bdde574355c8f43bb884e2cad158ce9d2fc5
Mario Martinez Leandro <mario.martinez@linio.com>
2015-01-21 09:12:43 -0600

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,10 +1,11 @@
 <?php
 	date_default_timezone_set("America/Mexico_City");
- 	echo date("Y/m/d H:i:s");
+ 	echo date("Y/m/d H:i:s")." Entrando a webhook<br/>\n";
 	// Use in the "Post-Receive URLs" section of your GitHub repo.
 	if ( $_POST['payload'] ) {
 		shell_exec( 'cd /srv/www/git-repo/ && git reset --hard HEAD && git pull');
+		echo "Ejecute el pull"
 	}
+	echo date("Y/m/d H:i:s")." Finalizando a webhook<br/>\n";
 ?>
-done 
 
