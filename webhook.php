05af2f4c29ee4ca4bfc6e19d186f0fea27432f0a
Mario Martinez Leandro <mario.martinez@linio.com>
2015-01-21 09:20:35 -0600

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -19,11 +19,11 @@ $commands = array(
 // Run the commands for output
 $output = '';
 foreach($commands AS $command){
-// Run it
-$tmp = shell_exec($command);
-// Output
-$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-$output .= htmlentities(trim($tmp)) . "\n";
+    // Run it
+    $tmp = shell_exec($command);
+    // Output
+    $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+    $output .= htmlentities(trim($tmp)) . "\n";
 }
 // Make it pretty for manual user access (and why not?)
 ?>
